c405c8b9993def27ebf9f1f251b4a1ae51bfc420
SeyoungOh <33598679+driffe@users.noreply.github.com>
2024-01-19 10:15:51 +0000

    change some css

--- weather-app/pages/index.tsx
+++ weather-app/pages/index.tsx
@@ -3,9 +3,8 @@ import { getWeatherData } from '../pages/api/hello';
 import WeatherData from '../pages/api/types';
 import { WiDaySunny, WiCloudy, WiRain, WiSnow, WiFog, WiThunderstorm } from 'react-icons/wi';
 
-
 export default function Home() {
-  const [city, setCity] = useState(''); // State to manage user input
+  const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
   const [error, setError] = useState<string | null>(null);
 
@@ -14,11 +13,10 @@ export default function Home() {
       if (city) {
         const data = await getWeatherData(city);
         setWeatherData(data);
-        setError(null); // Reset error state on successful fetch
+        setError(null);
       }
-    } catch (error:any) {
+    } catch (error: any) {
       setError(error.message || 'Error fetching weather data. Please try again.');
-      // Optionally, you can log the error here or handle it in a different way
     }
   };
 
@@ -61,30 +59,37 @@ export default function Home() {
       case '50n':
         return <WiFog />;
       default:
-        return null; // No specific icon for other conditions
+        return null;
     }
   };
 
   return (
-    <div className='flex flex-col align-center justify-center text-xl'>
-      <h1>Weather App</h1>
-      <input
-        type="text"
-        placeholder="Enter city"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
+    <div className='flex flex-col items-center justify-center text-xl h-svh bg-gray-200 text-gray-800'>
+      <h1 className="text-4xl mb-6 font-bold">Weather App</h1>
+      <div className="flex items-center">
+        <input
+          type="text"
+          placeholder="Enter city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          className='px-4 py-2 mr-3 bg-white border border-gray-300 focus:outline-none focus:border-gray-500 transition duration-300'
+        />
+        <button onClick={handleSearch} className='px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700 transition duration-300'>
+          Search
+        </button>
+      </div>
       {weatherData && (
-        <div>
-          <h2>{weatherData.weather[0].main}</h2>
-          <p>Description: {weatherData.weather[0].description}</p>
-          <p>Temperature: {weatherData.main.temp}°C</p>
+        <div className="mt-8 p-6 bg-white shadow-lg rounded-md">
+          <h2 className="text-2xl mb-2">{weatherData.weather[0].main}</h2>
+          <p className="mb-2">Description: {weatherData.weather[0].description}</p>
+          <p className="mb-2">Temperature: {weatherData.main.temp}°C</p>
           <p>Humidity: {weatherData.main.humidity}%</p>
-          {weatherData.weather[0].icon && getWeatherIcon(weatherData.weather[0].icon, 40)}
+          <div className="mt-4">
+            <p>Weather Icon: {getWeatherIcon(weatherData.weather[0].icon, 40)}</p>
+          </div>
         </div>
       )}
+      {error && <p className="mt-4 text-red-500">{error}</p>}
     </div>
-  )
+  );
 }
-
