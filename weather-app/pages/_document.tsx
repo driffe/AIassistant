6cd972493b430e827e61fbe83a597f1a7a46f575
SeyoungOh <33598679+driffe@users.noreply.github.com>
2024-02-06 14:03:40 +0000

    add pwa

--- weather-app/pages/_document.tsx
+++ weather-app/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <link rel="/manifest" href="/manifest.json" />
+      <link rel="manifest" href="../public/manifest.json" />
       <link
           href="/public/favicon-16x16.png"
           rel="icon"
