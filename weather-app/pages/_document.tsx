b1ff2eaf009f89e25126b3d5596f522e0fcfb8ca
SeyoungOh <33598679+driffe@users.noreply.github.com>
2024-02-06 14:09:14 +0000

    syntax error

--- weather-app/pages/_document.tsx
+++ weather-app/pages/_document.tsx
@@ -4,19 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <link rel="manifest" href="../public/manifest.json" />
-      <link
-          href="/public/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-      />
-      <link
-          href="images/favicons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-      />
+      <link rel="/manifest" href="manifest.json" />
       <body>
         <Main />
         <NextScript />
